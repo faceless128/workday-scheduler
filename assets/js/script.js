f4781f1a40f284859355c4c5ed7dc7eea5782896
Kaijam Jeffers <faceless128@gmail.com>
2022-03-13 17:14:06 -0400

    added edit boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,30 +5,11 @@ $(currentDay).text(todayIs);
 var timeBlocks = "";
 
 // textarea was clicked
-$(".schedule-item").on("click", "div", function() {
+$(".time-block").on("click", "section", function() {
     var text = $(this).text().trim();
     console.log(text);
-    var textInput = $("<textarea>").addClass("form-control").val(text);
+    var textInput = $("<textarea>").addClass("form-control col-8 col-md-10").val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
   });
   
-
-var makeDay = function() {
-    // var thisTime = $("<div>").addClass("hour col-2 col-md-1").val("8AM");
-    // console.log(thisTime);
-    // var thisData = $("<div>").addClass("present col-8 col-md-10").val("meeting");
-    // console.log(thisData);
-    // var saveThis = $("<span>").addClass("oi on-hard-drive");
-    // console.log(saveThis);
-    // var thisEdit = $("<div>").addClass("saveBtn col-2 col-md-1").val(saveThis);
-    // console.log(thisEdit);
-    // var timeBlocks = $("<div>").addClass("time-block row");
-    // console.log(timeBlocks);
-    // $(".scheduer").append(timeBlocks);    
-    // $(".time-block").append(thisTime);
-    // $(".time-block").append(thisData);
-    // $(".time-block").append(thisEdit);
-}
-
-makeDay();
