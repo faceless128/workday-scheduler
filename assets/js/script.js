a4bf9f123c08639b862ff73d285ef585542e7157
Kaijam Jeffers <faceless128@gmail.com>
2022-03-13 16:33:32 -0400

    added work day layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 /* put today's date in hero */
 var todayIs = moment().format("dddd, MMMM Do YYYY");
 
-console.log(todayIs);
+$(currentDay).text(todayIs);
+var timeBlocks = "";
 
-$(currentDay).text(todayIs)
+
+var makeDay = function() {
+    // var thisTime = $("<div>").addClass("hour col-2 col-md-1").val("8AM");
+    // console.log(thisTime);
+    // var thisData = $("<div>").addClass("present col-8 col-md-10").val("meeting");
+    // console.log(thisData);
+    // var saveThis = $("<span>").addClass("oi on-hard-drive");
+    // console.log(saveThis);
+    // var thisEdit = $("<div>").addClass("saveBtn col-2 col-md-1").val(saveThis);
+    // console.log(thisEdit);
+    // var timeBlocks = $("<div>").addClass("time-block row");
+    // console.log(timeBlocks);
+    // $(".scheduer").append(timeBlocks);    
+    // $(".time-block").append(thisTime);
+    // $(".time-block").append(thisData);
+    // $(".time-block").append(thisEdit);
+}
+
+makeDay();
