beafa3075dfdd637af4171498930f5c0a7324f68
Kaijam Jeffers <faceless128@gmail.com>
2022-03-13 19:39:09 -0400

    added past/present/future logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ var timeBlocks = "";
 // textarea was clicked
 $(".time-block").on("click", "section", function() {
     var text = $(this).text().trim();
-    console.log(text);
     var textInput = $("<textarea>").addClass("form-control col-8 col-md-10").val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
@@ -16,16 +15,23 @@ $(".time-block").on("click", "section", function() {
 // save button was clicked
 $(".saveBtn").on("click", function() {
     var saveButton = $(this).attr("id");
-    console.log(saveButton);
     var scheduleHour = $("." + saveButton).text().trim();
     var textareaSpot = $("." + saveButton).next();
     var textareaText = $("." + saveButton).next().val().trim();
-    console.log(scheduleHour);
-    console.log(textareaSpot);
-    console.log(textareaText);
     var textSection = $("<section>").addClass("col-8 col-md-10 schedule-item").text(textareaText);
-    console.log(textSection);
+    checkSchedule(saveButton, textSection);
     $(textareaSpot).replaceWith(textSection);
 });
 
-
+// check times on schedule 
+var checkSchedule = function (timeVar, textSection) {
+    var testTime = moment(timeVar, 'ha');
+    var test = Math.abs(moment().diff(testTime, "hours"));
+    if (moment().isAfter(testTime)) {
+        $(textSection).addClass("past");
+    } else if (Math.abs(moment().diff(testTime, "hours")) <= 1) {
+        $(textSection).addClass("present");
+    } else {
+        $(textSection).addClass("future");
+    }
+}
