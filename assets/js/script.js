41cb741cb69e468c2a3c6bc080704fa2491de4a4
Kaijam Jeffers <faceless128@gmail.com>
2022-03-13 18:46:34 -0400

    edit boxes can convert back to div

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,21 @@ $(".time-block").on("click", "section", function() {
     var textInput = $("<textarea>").addClass("form-control col-8 col-md-10").val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
-  });
+});
   
+// save button was clicked
+$(".saveBtn").on("click", function() {
+    var saveButton = $(this).attr("id");
+    console.log(saveButton);
+    var scheduleHour = $("." + saveButton).text().trim();
+    var textareaSpot = $("." + saveButton).next();
+    var textareaText = $("." + saveButton).next().val().trim();
+    console.log(scheduleHour);
+    console.log(textareaSpot);
+    console.log(textareaText);
+    var textSection = $("<section>").addClass("col-8 col-md-10 schedule-item").text(textareaText);
+    console.log(textSection);
+    $(textareaSpot).replaceWith(textSection);
+});
+
+
